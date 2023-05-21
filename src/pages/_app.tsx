0cf926e1a81c67e2015abadf687ed2b9375cb7b5
Mac32 <alberth.bompart22@gmail.com>
2023-05-20 22:33:51 -0500

    Correction of the letter in the word Element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 
-export default function MyApp ({ Component, pageProps }: AppProps): JSX.Elementy {
+export default function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Component {...pageProps} />
